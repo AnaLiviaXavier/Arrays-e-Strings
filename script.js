9cb62d98b740af006580f473c4ff7da276e4f6a1
Ana Xavier <analiviaxavier333@gmail.com>
2023-04-18 22:00:29 -0300

    Commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let array
+/*let array
 console.log('a. ', array)
 
 array = null
@@ -15,3 +15,28 @@ console.log('e. ', array)
 
 const valor = array[i+6]
 console.log('f ', valor)
+
+*/
+
+//const frase= prompt('Digite uma frase')
+//console.log(frase.toUpperCase().replaceAll("A","I"), frase.length)
+
+
+
+/*
+let nome = prompt("Digite seu nome:")
+let email = prompt('Digite seu email:')
+console.log('O e-mail '+ email + ' foi cadastrado com sucesso. \nSeja bem vindo(a), '+ nome + '!')
+*/
+
+
+let listaDeTarefas = []
+listaDeTarefas[0] = prompt('Qual a primeira tarefa que você precisa realizar no dia?')
+listaDeTarefas[1] = prompt('Qual a segunda tarefa que você precisa realizar no dia?')
+listaDeTarefas[2] = prompt('Qual a terceira tarefa que você precisa realizar no dia?')
+console.log(listaDeTarefas)
+
+let i = prompt('Digite o índice de uma tarefa que você já realizou: 0, 1 ou 2')
+listaDeTarefas.splice[i]
+console.log('Tarefa retirada: '+listaDeTarefas[i])
+console.log('Lista atualizada: '+listaDeTarefas)
